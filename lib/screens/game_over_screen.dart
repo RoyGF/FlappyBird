0a1480f561556f5f16694b0700d7412180d4da55
Roy <roygufo@gmail.com>
2024-04-16 21:47:06 -0600

    Add Game Over Screen

--- lib/screens/game_over_screen.dart
+++ lib/screens/game_over_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flappy_bird/core/asset_routes.dart';
 import 'package:flappy_bird/game/flappy_bird_game.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,37 @@ class GameOverScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      color: Colors.black38,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              // 'Score: ${game.bird.score}',
+              "Score: 0",
+              style: TextStyle(
+                fontSize: 60,
+                color: Colors.white,
+                fontFamily: 'Game',
+              ),
+            ),
+            const SizedBox(height: 20),
+            Image.asset(AssetRoutes.gameOver),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: onRestart,
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+              child: const Text(
+                'Restart',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
+  void onRestart() {}
 }
